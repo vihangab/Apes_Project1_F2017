9cc9b967e3fb050490e04a4d0c03cd79052deaa8
Virag Gada <virag.gada@colorado.edu>
2017-10-31 10:15:25 -0600

    Adding function definitions to I2C library - Feature Commit

--- i2c_lib.c
+++ i2c_lib.c
@@ -17,29 +17,63 @@ Description - Source file for out I2C library
 // Function to setup an I2C device
 i2c_state setupI2CDevice (uint32_t * file, char * device, uint8_t devAddr)
 {
-
+  if ((*file = open(device, O_RDWR)) < 0)
+  {
+    printf("Failed to open the i2c bus\n");
+    return ERROR;
+  }
+  else if (ioctl(*file, I2C_SLAVE, devAddr) < 0)
+    {
+      printf("Failed to acquire bus access and/or talk to slave.\n");
+      /* ERROR HANDLING; you can check errno to see what went wrong */
+      return ERROR;
+    }
+  return SUCCESS;
 }
 
 // Function to write a byte to an I2C device
 i2c_state writeI2CByte (uint32_t * file, int8_t* data)
 {
-
+  if (write(*file,data,1) != 1)
+  {
+    /* ERROR HANDLING: i2c transaction failed */
+    printf("Failed to write to the i2c bus.\n");
+    return ERROR;
+  }
+  return SUCCESS;
 }
 
-// Function to read a word from an I2C device
+// Function to read a byte from an I2C device
 i2c_state readI2CByte (uint32_t * file, int8_t * data)
 {
-
+  if(read(*file, data, 1)!=1)
+  {
+    printf("Failed to read in the buffer\n");
+    return ERROR;
+  }
+  return SUCCESS;
 }
 
 // Function to write a word to an I2C device
 i2c_state writeI2CWord(uint32_t * file, int8_t* data)
 {
-
+  if (write(*file,data,2) != 2)
+  {
+    /* ERROR HANDLING: i2c transaction failed */
+    printf("Failed to write to the i2c bus.\n");
+    return ERROR;
+  }
+  return SUCCESS;
 }
 
 // Function to read a word from an I2C device
 i2c_state readI2CWord (uint32_t * file, int8_t * data)
 {
-
+  if(read(*file, data, 2)!=2)
+  {
+    printf("Failed to read in the buffer\n");
+    return ERROR;
+  }
+  return SUCCESS;
 }
+
