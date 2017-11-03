addf90e12df585751edc83c96b399e187e207dde
Virag Gada <virag.gada@colorado.edu>
2017-11-03 15:27:26 -0600

    Adding function definitions - Feature commit

--- tmp102.c
+++ tmp102.c
@@ -1,58 +1,245 @@
+/****************************************************
+Name - tmp102.c
+Author - Virag Gada and Vihanga Bare
+Description - Source file for out TMP 102 library
+*****************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "i2c_lib.h"
 #include "tmp106.h"
 
-i2c_state write_tmp106_registers()
+i2c_state setup_tmp102_registers(uint32_t *file)
 {
-	// set THigh register threshold limits
-	// set TLow register threshold limits
-	
-	//call to write_tmp106_config()
-	//call to write_tmp106_pointer_reg()
+  if(write_tmp102_config(file,INTERRUPT_MODE,EXTENDED_MODE|CONVERSION_RATE_8)==ERROR_WRITE){
+    return ERROR_WRITE;
+	}else if(set_low_threshold_c(file,20.0)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}else if(set_high_threshold_c(file,22.0)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}
+	return SUCCESS;
 }
 
-i2c_state read_tmp106_registers()
+i2c_state write_tmp102_config(uint32_t *file, uint8_t msb, uint8_t lsb)
 {
-	//call to read_tmp106_config()
-	
-	// read THigh register
-	// read TLow register
-	
-	// this is required as we need to read T LOW and T High and perform necessary calculations for *C, *F and *K values using these values
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	uint8_t reg_value[2];
+	/* Check previous values */
+  if(read_tmp102_config(file,reg_value) == ERROR_READ){
+		return ERROR_READ;
+	}
+  reg_value[0] |= msb;
+	reg_value[1] |= lsb;
+  return writeI2CWord(file,reg_value);
 }
 
-i2c_state write_tmp106_config()
+i2c_state read_tmp102_config(uint32_t *file, uint8_t* data)
 {
-
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	if(write_tmp102_pointer_reg(file,SELECT_CONFIG) == ERROR_WRITE){
+		return ERROR_WRITE;
+	}else if(readI2CWord(file,data) == ERROR_READ){
+		return ERROR_READ;
+	}
+	return SUCCESS;
 }
 
-i2c_state read_tmp106_config()
+i2c_state write_tmp102_pointer_reg(uint32_t *file, uint8_t reg_addr)
 {
-
-}
-i2c_state write_tmp106_pointer_reg()
-{
-	
-}
-
-i2c_state read_tmp106_Temp_reg()
-{
-	//read from Temperature register which is read--only
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	return writeI2CByte(file, &reg_addr);
 }
 
-i2c_state pwr_up_sequence_tmp106()
+i2c_state read_tmp102_temp_reg_c(uint32_t *file,double *digitalTemp)
 {
-	//call to i2c_state write_tmp106_registers() to write appropriate registers 
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	//read from Temperature register which is read-only
+  uint8_t reg_value[2];
+	int16_t intTemp;
+	if(write_tmp102_pointer_reg(file,SELECT_TEMP)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}else if(readI2CWord(file,reg_value)==ERROR_READ){
+		return ERROR_READ;
+	}
+	/* Check resolution and find value */
+	if(reg_value[1]&0x01){//13 bit mode
+    intTemp = (reg_value[0]<<5)|(reg_value[1]>>3);
+		/* Take 2's compliment if negative*/
+		if(intTemp > 0xFFF){
+			intTemp |= 0xE000;
+		}
+	}else{ // 12 bit mode
+		intTemp = (reg_value[0]<<4)|(reg_value[1]>>4);
+		/* Take 2's compliment if negative*/
+		if(intTemp > 0x7FF){
+			intTemp |= 0xF000;
+		}
+	}
+  *digitalTemp = 0.0625*intTemp;
+	return SUCCESS;
 }
 
-i2c_state pwr_down_sequence_tmp106()
-{
-	//call to i2c_state write_tmp106_registers() to write appropriate registers 
+i2c_state read_tmp102_temp_reg_f(uint32_t *file,double *digitalTemp){
+  return read_tmp102_temp_reg_c(file,digitalTemp)*9.0/5.0+32.0;
 }
-i2c_state set_resolution_tmp106()
+
+i2c_state read_tmp102_temp_reg_k(uint32_t *file,double *digitalTemp){
+  return read_tmp102_temp_reg_c(file,digitalTemp)+273.15;
+}
+
+i2c_state pwr_up_tmp102(uint32_t *file)
 {
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	uint8_t prev_value;
+	if(write_tmp102_pointer_reg(file,SELECT_CONFIG)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}else if(readI2CByte(file,&prev_value)==ERROR_READ){
+		return ERROR_READ;
+	}
+  prev_value = prev_value & (!ENABLE_SHUTDOWN);
+	if(writeI2CByte(file,&prev_value)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}
+  return SUCCESS;
+}
+
+i2c_state pwr_down_tmp102(uint32_t *file)
+{
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	uint8_t prev_value;
+	if(write_tmp102_pointer_reg(file,SELECT_CONFIG)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}else if(readI2CByte(file,&prev_value)==ERROR_READ){
+		return ERROR_READ;
+	}
+  prev_value = prev_value | ENABLE_SHUTDOWN;
+	if(writeI2CByte(file,&prev_value)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}
+  return SUCCESS;
+}
+
+i2c_state set_resolution_tmp102(uint32_t *file, uint8_t mode)
+{
+	if(file == NULL){
+		return NULL_POINTER;
+	}
 	//set the resolution for tmp 106
+	uint8_t prev_value[2];
+	if(read_tmp102_config(file,prev_value)==ERROR_READ){
+		return ERROR_READ;
+	}
+  prev_value[1] = prev_value[1] & (!EXTENDED_MODE); //Clear previous mode
+	prev_value[1] = prev_value[1] | mode;
+	if(writeI2CWord(file,prev_value)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}
+	return SUCCESS;
+}
+
+i2c_state set_low_threshold_c(uint32_t *file, float temp){
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	uint8_t extendedMode;
+	uint8_t reg_value[2];
+
+	if(temp > 150.0f)
+	{
+		temp = 150.0f;
+	}
+	if(temp < -55.0)
+	{
+		temp = -55.0f;
+	}
+	if(read_tmp102_config(file,reg_value)==ERROR_READ){
+		return ERROR_READ;
+	}
+	extendedMode = EXTENDED_MODE & reg_value[1];
+	temp = temp / 0.0625;
+
+	if(extendedMode){ //13 bit mode
+		reg_value[0] = (uint32_t)(temp)>>5;
+		reg_value[1] = (uint32_t)(temp)<<3;
+	}else{ //12 bit mode
+		reg_value[0] = (uint32_t)(temp)>>4;
+		reg_value[1] = (uint32_t)(temp)<<4;
+	}
+	if(write_tmp102_pointer_reg(file,SELECT_TLOW) == ERROR_WRITE){
+		return ERROR_WRITE;
+	}else if(writeI2CWord(file,reg_value) == ERROR_WRITE){
+		return ERROR_WRITE;
+	}
+	return SUCCESS;
 }
 
 
+i2c_state set_high_threshold_c(uint32_t *file, float temp){
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	uint8_t extendedMode;
+	uint8_t reg_value[2];
+
+	if(temp > 150.0f)
+	{
+		temp = 150.0f;
+	}
+	if(temp < -55.0)
+	{
+		temp = -55.0f;
+	}
+	if(read_tmp102_config(file,reg_value)==ERROR_READ){
+		return ERROR_READ;
+	}
+	extendedMode = EXTENDED_MODE & reg_value[1]; //0x10 - 13 bit mode
+	                                             //0x00 - 12 bit mode
+	temp = temp / 0.0625;
+
+	if(extendedMode){ //13 bit mode
+		reg_value[0] = (uint32_t)(temp)>>5;
+		reg_value[1] = (uint32_t)(temp)<<3;
+	}else{ //12 bit mode
+		reg_value[0] = (uint32_t)(temp)>>4;
+		reg_value[1] = (uint32_t)(temp)<<4;
+	}
+	if(write_tmp102_pointer_reg(file,SELECT_THIGH) == ERROR_WRITE){
+		return ERROR_WRITE;
+	}else if(writeI2CWord(file,reg_value) == ERROR_WRITE){
+		return ERROR_WRITE;
+	}
+	return SUCCESS;
+}
+
+
+i2c_state set_conversion_rate(uint32_t *file,uint32_t value)
+{
+	if(file == NULL){
+		return NULL_POINTER;
+	}
+	uint8_t prev_value[2];
+	if(write_tmp102_pointer_reg(file,SELECT_CONFIG)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}else if(readI2CWord(file,prev_value)==ERROR_READ){
+		return ERROR_READ;
+	}
+  prev_value[1] = prev_value[1] & 0xC0;
+  prev_value[1] = prev_value[1] | value;
+	if(writeI2CWord(file,prev_value)==ERROR_WRITE){
+		return ERROR_WRITE;
+	}
+  return SUCCESS;
+}
--- tmp102.h
+++ tmp102.h
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdint.h>
 
-#ifndef TMP106_H_
-#define TMP106_H_
+#ifndef TMP102_H_
+#define TMP102_H_
 
-#define ALERTbit          (0x00) // Comparator Mode = 00
-#define POWER_VCC         (0x49) //VCC
-#define SDA_PIN           (0x4A) //SDA_PIN
-#define SDL_PIN           (0x4B) //SDL_PIN
+#define ALERTbit             (0x00) // Comparator Mode = 00
+#define POWER_VCC            (0x49) //VCC
+#define SDA_PIN              (0x4A) //SDA_PIN
+#define SDL_PIN              (0x4B) //SDL_PIN
+#define SLAVE_ADDR           (0x48)
 
-//P1,P0 bit in Pointer Register of TMP 106 to select registers
+#define POINTER_MASK			   (0x03)
 
-#define TEMP_SELECT_ROTEMP	(0x00)
-#define TEMP_SELECT_CONFIG	(0x01)
-#define TEMP_SELECT_TLOW	(0x20)
-#define TEMP_SELECT_THIGH	(0x03)
+//P1,P0 bit in Pointer Register of TMP 102 to select registers
+#define SELECT_TEMP          (0x00)
+#define SELECT_CONFIG	       (0x01)
+#define SELECT_TLOW	         (0x20)
+#define SELECT_THIGH	       (0x03)
 
+/* Config register values */
+#define ENABLE_SHUTDOWN      (0x01)
+#define DISABLE_SHUTDOWN     (0x00)
+#define COMPARATOR_MODE      (0x00)
+#define INTERRUPT_MODE       (0x02)
+#define POLARITY_ZERO        (0x00)
+#define POLARITY_ONE         (0x04)
+#define FAULTS_ONE           (0x00)
+#define FAULTS_TWO           (0x08)
+#define FAULTS_FOUR          (0x10)
+#define FAULTS_SIX           (0x18)
+#define ONE_SHOT					   (0x80)
+#define NORMAL_MODE          (0x00)
+#define EXTENDED_MODE        (0x10)
+#define CONVERSION_RATE_0_25 (0x00)
+#define CONVERSION_RATE_1    (0x40)
+#define CONVERSION_RATE_4    (0x80)
+#define CONVERSION_RATE_8    (0xC0)
 
 struct temp_data
 {
@@ -24,25 +45,31 @@ struct temp_data
 	double temp_celsius;
 	double temp_farenh;
 	double temp_kelvin;
-	
 }Temp_Data;
 
-i2c_state write_tmp106_registers();
+i2c_state setup_tmp102_registers(uint32_t *file);
 
-i2c_state read_tmp106_registers();
+i2c_state write_tmp102_config(uint32_t *file, uint8_t msb, uint8_t lsb);
 
-i2c_state write_tmp106_config();
+i2c_state read_tmp102_config(uint32_t *file, uint8_t* data);
 
-i2c_state read_tmp106_config();
+i2c_state write_tmp102_pointer_reg(uint32_t *file, uint8_t reg_addr);
 
-i2c_state write_tmp106_pointer_reg();
+i2c_state read_tmp102_temp_reg_c(uint32_t *file,double *digitalTemp);
 
-i2c_state read_tmp106_Temp_reg();
+i2c_state read_tmp102_temp_reg_f(uint32_t *file,double *digitalTemp);
 
-i2c_state pwr_up_sequence_tmp106();
+i2c_state read_tmp102_temp_reg_k(uint32_t *file,double *digitalTemp);
 
-i2c_state pwr_down_sequence_tmp106();
+i2c_state pwr_up_tmp102(uint32_t *file);
 
-i2c_state set_resolution_tmp106();
+i2c_state pwr_down_tmp102(uint32_t *file);
 
+i2c_state set_resolution_tmp102(uint32_t *file, uint8_t mode);
+
+i2c_state set_low_threshold_c(uint32_t *file, float temp);
+
+i2c_state set_high_threshold_c(uint32_t *file, float temp);
+
+i2c_state set_conversion_rate(uint32_t *file,uint32_t value);
 #endif /* TMP106_H_ */
