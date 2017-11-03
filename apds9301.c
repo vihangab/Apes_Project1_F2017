cc2986a36f52d2a7d5f3a44bee27923d8be1cddf
Virag Gada <virag.gada@colorado.edu>
2017-11-02 18:12:20 -0600

    Adding Functiton definitions - Feature Commit

--- apds9301.c
+++ apds9301.c
@@ -1,64 +1,160 @@
+/****************************************************
+Name - apds9301.c
+Author - Virag Gada and Vihanga Bare
+Description - Source file for out APDS 9301 library
+*****************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "i2c_lib.h"
 #include "apds9301.h"
 
 // Function to write all registers
-i2c_state write_registers()
+i2c_state write_registers(uint32_t * file)
 {
-  //Also calls config register
+  //Calls control and timing register
+  i2c_state status;
+  if((status = write_control(file,POWER_ON)) == ERROR){
+    return status;
+  }else if((status = config_integration(file,INTEG_TIME_101)) == ERROR){
+    return status;
+  }else {
+    uint8_t data[2]={0};
+    data[0] = CMDbit | TLL_ADDR;
+    data[1] = TLL;
+    if((status = writeI2CByte(file,data)) == ERROR){
+      return status;
+    }
+    data[0] = CMDbit | TLH_ADDR;
+    data[1] = TLH;
+    if((status = writeI2CByte(file,data)) == ERROR){
+      return status;
+    }
+    data[0] = CMDbit | THL_ADDR;
+    data[1] = THL;
+    if((status = writeI2CByte(file,data)) == ERROR){
+      return status;
+    }
+    data[0] = CMDbit | THH_ADDR;
+    data[1] = THH;
+    if((status = writeI2CByte(file,data)) == ERROR){
+      return status;
+    }
+  }
+  return SUCCESS;
 }
 
 //Function to read all registers
 i2c_state read_registers()
 {
-
+  return 0;
 }
 
-// Function to write to config register
-i2c_state write_config()
+// Function to write to control register
+i2c_state write_control(uint32_t * file, uint8_t powerState)
 {
-
+  uint8_t data[2]={0};
+  data[0] = CTRL_ADDR | CMDbit;
+  data[1] = powerState;
+  return writeI2CWord(file,data);
 }
 
-// Function to read to config register
-i2c_state read_config()
+// Function to read to control register
+i2c_state read_control(uint32_t * file, uint8_t * powerState)
 {
-
+  uint8_t addr = CTRL_ADDR | CMDbit;
+  if(writeI2CByte(file,&addr)==ERROR){
+    return ERROR;
+  }else{
+    return readI2CByte(file,powerState);
+  }
+  return SUCCESS;
 }
 
 // Function to set integration times
-i2c_state config_integration(uint8_t integrationPeriod)
+i2c_state config_integration(uint32_t * file, uint8_t integrationPeriod)
 {
-
+  uint8_t data[2]={0};
+  data[0] = TIMING_ADDR | CMDbit;
+  data[1] = integrationPeriod;
+  return writeI2CWord(file,data);
 }
 
 // Function to enable the APDS sensor interrupt
-i2c_state enable_apds_interrupt()
+i2c_state enable_apds_interrupt(uint32_t * file)
 {
-
+  uint8_t data[2]={0};
+  data[0] = INT_ADDR | CMDbit;
+  data[1] = INTERRUPT_ENABLE | PERSIST;
+  return writeI2CWord(file,data);
 }
 
 //Function to disable APDS sensor interrupts
-i2c_state disable_apds_interrupt()
+i2c_state disable_apds_interrupt(uint32_t * file)
 {
-
+  uint8_t data[2]={0};
+  data[0] = INT_ADDR | CMDbit;
+  data[1] = INTERRUPT_DISABLE;
+  return writeI2CWord(file,data);
 }
 
 //Function to read the ID register
-i2c_state read_ID_reg(uint8_t *id_val)
+i2c_state read_ID_reg(uint32_t * file,uint8_t *id_val)
 {
-
+  uint8_t addr = ID_ADDR | CMDbit;
+  if(writeI2CByte(file,&addr)==ERROR){
+    return ERROR;
+  }else
+    return readI2CByte(file,id_val);
+  return SUCCESS;
 }
 
 // Function to read LUX values
-i2c_state read_lux_values(uint16_t *ch0, uint16_t *ch1)
+i2c_state read_lux_values(uint32_t * file, double * lux_value)
 {
+  uint8_t ch0_data[2] = {0}, ch1_data[2] = {0};
+  double ch0, ch1, ratio;
+  uint8_t addr_0 =  ADC0_DLOW_ADDR | CMDbit | Word_mode;
+  uint8_t addr_1;
+  //addr_1[0] = 0x39;
+  addr_1 =  ADC1_DLOW_ADDR | CMDbit | Word_mode;
+  if(writeI2CByte(file,&addr_0)==ERROR){
+    //printf("Error 1\n");
+    return ERROR;
+  }else if(readI2CWord(file,ch0_data)==ERROR){
+    //printf("Error 2\n");
+    return ERROR;
+  }else if(writeI2CByte(file, &addr_1)==ERROR){
+    //printf("Error 3\n");
+    return ERROR;
+  }else if(readI2CWord(file,ch1_data)==ERROR){
+    //printf("Error 4\n");
+    return ERROR;
+  }
 
+  printf("%s\n","Sent command");
+  ch0 = ch0_data[1]<<8 | ch0_data[0];
+  ch1 = ch1_data[1]<<8 | ch1_data[0];
+  printf("CH0 - %f, CH1 - %f\n", ch0,ch1);
+  ratio = ch1/ch0;
+  printf("Ratio - %f\n", ratio);
+  if((ratio)>0 && (ratio<=0.5)){
+    *lux_value = (0.0304*ch0)-(0.062*ch0*pow(ratio,1.4));
+  }else if(ratio<=0.61){
+    *lux_value = (0.0224*ch0)-(0.031*ch1);
+  }else if(ratio<=0.80){
+    *lux_value = (0.0128*ch0)-(0.0153*ch1);
+  }else if(ratio<=1.3){
+    *lux_value = (0.00146*ch0)-(0.00112*ch1);
+  }else {
+    *lux_value = 0;
+  }
+  return SUCCESS;
 }
 
 //Function to find light state based on luc values
 apds_state read_light_state()
 {
-
+ return 0;
 }
--- apds9301.h
+++ apds9301.h
@@ -1,36 +1,56 @@
 #include <stdio.h>
 #include <stdint.h>
+#include "i2c_lib.h"
 
 #ifndef APDS9301_H_
 #define APDS9301_H_
 
-#define CMDbit          (0x80)
-#define Word_mode       (0xC0)
+#include "i2c_lib.h"
+
+#define CMDbit           (0x80)
+#define Word_mode        (0xC0)
 
 // Address of all the registers
-#define I2C_CTRL_ADD    (0x00)
-#define I2C_TIMING      (0x01)
-#define I2C_TLL_ADD     (0x02)
-#define I2C_TLH_ADD     (0x03)
-#define I2C_THL_ADD     (0x04)
-#define I2C_THH_ADD     (0x05)
-#define I2C_INT_ADD     (0x06)
-#define ADC0_DLOW_ADD   (0x0C)
-#define ADC1_DLOW_ADD   (0x0E)
-#define ADC0_DHIGH_ADD  (0x0D)
-#define ADC1_DHIGH_ADD  (0x0F)
+#define CTRL_ADDR        (0x00)
+#define TIMING_ADDR      (0x01)
+#define TLL_ADDR         (0x02)
+#define TLH_ADDR         (0x03)
+#define THL_ADDR         (0x04)
+#define THH_ADDR         (0x05)
+#define INT_ADDR         (0x06)
+#define ID_ADDR          (0x0A)
+#define ADC0_DLOW_ADDR   (0x0C)
+#define ADC1_DLOW_ADDR   (0x0E)
+#define ADC0_DHIGH_ADDR  (0x0D)
+#define ADC1_DHIGH_ADDR  (0x0F)
 
-#define POWER           (0x03)
-#define TIMING          (0x01)      //Low gain
+#define POWER_ON         (0x03)
+#define POWER_OFF        (0x00)
 
+/* Timing Register definitions */
+#define HIGH_GAIN        (0x10)
+#define LOW_GAIN         (0x00)
+
+#define MANUAL_ON        (0x08)
+#define MANUAL_OFF       (0x00)
+
+#define INTEG_TIME_13    (0x00)
+#define INTEG_TIME_101   (0x01)
+#define INTEG_TIME_402   (0x02)
+#define INTEG_TIME_NA    (0x03)
+
+/* Intrerrupt Threshold Registers */
 // Low-High threshold values
-#define TLL             (0x0F)
-#define TLH             (0x00)
-#define THL             (0x00)
-#define THH             (0x08)
+#define TLL              (0x0F)
+#define TLH              (0x00)
+#define THL              (0x00)
+#define THH              (0x08)
 
-#define PERSIST         (0x04)
-#define INTR_FIELD      (0x01)
+/* Interrupt Control Register definitions */
+#define PERSIST          (0x04)
+
+#define INTERRUPT_ENABLE  (0x20)
+#define INTERRUPT_DISABLE (0x00)
 
 typedef enum{
   LIGHT,
@@ -40,26 +60,26 @@ typedef enum{
 typedef struct light_data
 {
   uint16_t lumen;
-  Light_State state;
+  apds_state state;
 }Light_Data;
 
-i2c_state write_registers();
+i2c_state write_registers(uint32_t * file);
 
 i2c_state read_registers();
 
-i2c_state write_config();
+i2c_state write_control(uint32_t * file, uint8_t powerState);
 
-i2c_state read_config();
+i2c_state read_control(uint32_t * file, uint8_t * powerState);
 
-i2c_state config_integration(uint8_t integrationPeriod);
+i2c_state config_integration(uint32_t * file, uint8_t integrationPeriod);
 
 i2c_state enable_apds_interrupt();
 
 i2c_state disable_apds_interrupt();
 
-i2c_state read_ID_reg(uint8_t *id_val);
+i2c_state read_ID_reg(uint32_t * file,uint8_t *id_val);
 
-i2c_state read_lux_values(uint16_t *ch0, uint16_t *ch1);
+i2c_state read_lux_values(uint32_t * file, double * lux_value);
 
 apds_state read_light_state();
 
