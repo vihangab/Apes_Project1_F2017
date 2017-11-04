193d57699d3baf5443ed766fd9be152b2278fa19
vihangab <bare.vihanga5@gmail.com>
2017-11-03 23:37:08 -0600

    Adding main
    Adding main

---
+++ main.c
@@ -0,0 +1,57 @@
+#include "msg_queue.h"
+
+int main()
+{
+	flag_mask = 0;
+	flag_mask_copy = 0;
+	int32_t retval = 0;
+	initialize_queue();
+	signal(SIGINT, sighandler_sigint);
+
+	pthread_create(&tempThread,NULL,&TempThread,NULL);
+	pthread_create(&lightThread,NULL,&LightThread,NULL);
+	pthread_create(&loggerThread,NULL,&LoggerThread,NULL);
+	pthread_create(&sighandThread,NULL,&SighandThread,NULL); //common signal handler for all events
+
+	create_timer(1.5);
+	
+	retval = pthread_join(tempThread,NULL);
+	if(retval != 0)
+	{
+		printf("pthread join failed, error code - %d\n", retval);
+	}
+	
+	retval = pthread_join(lightThread,NULL);
+	if(retval != 0)
+	{
+		printf("pthread join failed, error code - %d\n", retval);
+	}
+	
+	retval = pthread_join(loggerThread,NULL);
+	if(retval != 0)
+	{
+		printf("pthread join failed, error code - %d\n", retval);
+	}
+	
+	retval = pthread_join(sighandThread,NULL);
+	if(retval != 0)
+	{
+		printf("pthread join failed, error code - %d\n", retval);
+	}
+	
+	retval = pthread_mutex_destroy(&dataQ_mutex);
+	if(retval != 0)
+	{
+		printf("mutex destroy failed, error code - %d\n", retval);
+	}
+	
+	retval =  pthread_cond_destroy(&condvar);
+	if(retval != 0)
+	{
+		printf("cond destroy failed, error code - %d\n", retval);
+	}
+	
+	printf("Joined all threads, destroyed mutexes and condition vars...\n");
+	printf("Return zero from main()...\n");
+	return 0;
+}
